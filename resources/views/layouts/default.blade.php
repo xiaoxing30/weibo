6c2150e089959fbff16f9fbc010003f7f848dc50
xiaoxing30 <1137906361@qq.com>
2019-11-20 13:39:33 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>@yield('title','Weibo App') - xiaoxing30 </title>
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
+//.version()为每一次的文件修改做哈希处理。
+// 只要文件修改，哈希值就会变，提醒客户端需要重新加载文件，很巧妙地解决了静态文件缓存。
+// 解决文件版本变更。
