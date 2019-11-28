81c176e6fc05cce3762c145efae2a5d445ffa708
xiaoxing30 <1137906361@qq.com>
2019-11-28 09:48:18 +0000

    Move user model to models folder

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
     }
+
 }
