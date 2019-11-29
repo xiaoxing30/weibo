ebd112bc349337b3c1edfc41894d990ebacfb033
xiaoxing30 <1137906361@qq.com>
2019-11-29 12:30:56 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,13 +10,19 @@ class UsersController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth',[
-            'except' => ['show','create','store']
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
-        $this->middleware('guest',[
+        $this->middleware('guest', [
             'only' => ['create']
         ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
     public function create()
@@ -26,51 +32,51 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
     }
 
     public function store(Request $request)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password'=>'required|confirmed|min:6',
+            'password' => 'required|confirmed|min:6',
         ]);
 
         $user = User::create([
             'name' => $request->name,
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password),
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
         ]);
 
         Auth::login($user);
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 
     public function edit(User $user)
     {
-        $this->authorize('update',$user);
-        return view('users.edit',compact('user'));
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
     }
 
-    public function update(User $user,Request $request)
+    public function update(User $user, Request $request)
     {
-        $this->authorize('update',$user);
-        $this->validate($request,[
+        $this->authorize('update', $user);
+        $this->validate($request, [
             'name' => 'required|max:50',
-            'password'=>'nullable|confirmed|min:6'
+            'password' => 'nullable|confirmed|min:6'
         ]);
 
         $data = [];
         $data['name'] = $request->name;
-        if($request->password){
+        if ($request->password) {
             $data['password'] = bcrypt($request->password);
         }
         $user->update($data);
 
-        session()->flash('success','个人资料更新成功！');
+        session()->flash('success', '个人资料更新成功！');
 
-        return redirect()->route('users.show',$user->id);
+        return redirect()->route('users.show', $user->id);
     }
 }
