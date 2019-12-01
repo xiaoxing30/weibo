6584bdd20c8463a02d2895cf5806b4309e18e820
xiaoxing30 <1137906361@qq.com>
2019-12-01 08:16:35 +0000

    发布微博

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class StatusesController extends Controller
+{
+    public function __construct()
+    {
+        //这两个动作都需要用户登录，因此让我们借助 Auth 中间件来为这两个动作添加过滤请求。
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'content' => 'required|max:140'
+        ]);
+
+        Auth::user()->statuses()->create([
+            'content' => $request['content']
+        ]);
+        session()->flash('success','发布成功！');
+        return redirect()->back();
+    }
+}
